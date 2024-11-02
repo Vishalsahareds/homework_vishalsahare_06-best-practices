61989595b573ad87491aba5066a6fd8554f9fc59
Vishalsahareds <108393284+Vishalsahareds@users.noreply.github.com>
2024-11-02 15:44:30 +0000

    Vishal homework answer 5

--- project/batch.py
+++ project/batch.py
@@ -1,3 +1,5 @@
+
+
 import os
 import pandas as pd
 
@@ -13,14 +15,20 @@ def read_data(file_path):
     return df
 
 def save_data(df, file_path):
+    options = {
+        'client_kwargs': {
+            'endpoint_url': S3_ENDPOINT_URL
+        }
+    }
     df.to_parquet(file_path, engine='pyarrow', compression=None, index=False, storage_options=options)
 
 if __name__ == "__main__":
     year = 2023
-    month = 3
+    month = 1
     input_file = f"s3://nyc-duration/in/{year:04d}-{month:02d}.parquet"
     output_file = f"s3://nyc-duration/out/{year:04d}-{month:02d}.parquet"
 
     df = read_data(input_file)
     # Perform some operations on df if needed
     save_data(df, output_file)
+
--- project/integration_test.py
+++ project/integration_test.py
@@ -19,12 +19,11 @@ def get_output_path(year, month):
     output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
     return output_pattern.format(year=year, month=month)
 
+def read_data(file_path):
+    df = pd.read_parquet(file_path, storage_options=options)
+    return df
+
 def save_data(df, file_path):
-    options = {
-        'client_kwargs': {
-            'endpoint_url': S3_ENDPOINT_URL
-        }
-    }
     df.to_parquet(file_path, engine='pyarrow', compression=None, index=False, storage_options=options)
 
 def main(year, month):
@@ -32,10 +31,17 @@ def main(year, month):
     output_file = get_output_path(year, month)
 
     # Create a dataframe (pretend this is data for January 2023)
+    # data = {
+    #     'column1': [1, 2, 3],
+    #     'column2': ['a', 'b', 'c'],
+    #     'predicted_duration': [13.08, 36.28, 69.28]
+    # }
+
     data = {
-        'column1': [1, 2, 3],
-        'column2': ['a', 'b', 'c']
+        'column2': ['b'],
+        'predicted_duration': [36.28]
     }
+
     df_input = pd.DataFrame(data)
 
     # Save dataframe to S3
@@ -51,5 +57,9 @@ def main(year, month):
     print("Data read from S3:")
     print(df_output)
 
+    # Calculate the sum of predicted durations
+    sum_predicted_durations = df_output['predicted_duration'].sum()
+    print(f"Sum of predicted durations: {sum_predicted_durations}")
+
 if __name__ == "__main__":
-    main(2023, 3)
+    main(2023, 1)
